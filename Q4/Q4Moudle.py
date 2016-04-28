d5aac6acadc9fa68f1f25b1f7382d27a80a035e1
len <len831012@gmail.com>
2016-04-29 00:12:27 +0800

    add my sourcecode

--- Q4/Q4Moudle.py
+++ Q4/Q4Moudle.py
@@ -128,20 +128,20 @@ class UnorderedList:
 if __name__ == "__main__":
     mylist = UnorderedList()
     print(mylist.isEmpty())
-    mylist.add(3323)
-    mylist.add('melon')
+    mylist.add(1234)
+    mylist.add('pig')
     mylist.add(1024)
-    print(mylist.size())  # ['melon',3323,1024] This order may not be
-    mylist.append(555)
-    print(mylist.search(555))  # ['melon',3323,1024,555]
     print(mylist.size())
-    mylist.remove(1024)  # ['melon',3323,555]
+    mylist.append(7777)
+    print(mylist.search(7777))
     print(mylist.size())
-    print(mylist.index(555))
-    mylist.insert(2, 44)  # ['melon',3323,44,555]
-    print(mylist.index(44))
-    mylist.pop()  # ['melon',3323,44]
-    print(mylist.search(555))
-    mylist.pop(1)  # ['melon',44]
-    print(mylist.search(3323))
+    mylist.remove(1024)
+    print(mylist.size())
+    print(mylist.index(7777))
+    mylist.insert(2, 33)
+    print(mylist.index(33))
+    mylist.pop()
+    print(mylist.search(7777))
+    mylist.pop(1)
+    print(mylist.search(1234))
 
